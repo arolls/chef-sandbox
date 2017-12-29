823d9184b02c30c0386d5aff4d7d8cf90b9566b0
arolls <alex.rolls@skybettingandgaming.com>
2017-12-29 15:21:14 +0000

    Added mysql.

--- cookbooks/lamp/metadata.rb
+++ cookbooks/lamp/metadata.rb
@@ -11,6 +11,9 @@ source_url 'https://github.com/learn-chef/lamp' if respond_to?(:source_url)
 supports 'ubuntu'
 
 depends 'httpd', '~> 0.4'
+depends 'mysql', '~> 8.0'
+depends 'mysql2_chef_gem', '~> 2.0'
+depends 'database', '~> 6.1'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
