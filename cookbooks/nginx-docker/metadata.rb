b2d341cc38522eca95fa2904f36f01c2a7d2a7c1
arolls <alex.rolls@skybettingandgaming.com>
2018-01-12 23:29:51 +0000

    v0.4.0

--- cookbooks/nginx-docker/metadata.rb
+++ cookbooks/nginx-docker/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures nginx-docker'
 long_description 'Installs/Configures nginx-docker'
-version '0.3.0'
+version '0.4.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'docker', '~> 3.0'
